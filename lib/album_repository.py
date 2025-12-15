ae237d477ffcd83996269e1bfd93bafbc062eeb5
sarahmcvay <tosarah.s@gmail.com>
2025-12-15 17:33:58 +0000

    album repository class created

---
+++ lib/album_repository.py
@@ -0,0 +1,14 @@
+from lib.album import *
+
+class AlbumRepository():
+
+    def __init__(self, connection):
+        self._connection = connection 
+
+    def all(self): 
+        rows = self._connection.execute('SELECT * from albums')
+        albums = []
+        for row in rows: 
+            item = Album(row["title"], row["release_year"], row["artist_id"])
+            albums.append(item)
+        return albums
