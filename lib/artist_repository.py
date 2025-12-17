f0c587322212763adc4dc97e34fab81dadfa51f5
sarahmcvay <tosarah.s@gmail.com>
2025-12-17 09:01:35 +0000

    minor refactor for ease of reading

--- lib/artist_repository.py
+++ lib/artist_repository.py
@@ -11,26 +11,40 @@ class ArtistRepository:
         rows = self._connection.execute('SELECT * from artists')
         artists = []
         for row in rows:
-            item = Artist(row["id"], row["name"], row["genre"])
+            item = Artist(
+                row["id"], 
+                row["name"], 
+                row["genre"]
+                )
             artists.append(item)
         return artists
 
     # Find a single artist by their id
     def find(self, artist_id):
         rows = self._connection.execute(
-            'SELECT * from artists WHERE id = %s', [artist_id])
+            'SELECT * from artists WHERE id = %s', 
+            [artist_id]
+            )
         row = rows[0]
-        return Artist(row["id"], row["name"], row["genre"])
+        return Artist(
+            row["id"], 
+            row["name"], 
+            row["genre"]
+            )
 
     # Create a new artist
     # Do you want to get its id back? Look into RETURNING id;
     def create(self, artist):
-        self._connection.execute('INSERT INTO artists (name, genre) VALUES (%s, %s)', [
-            artist.name, artist.genre])
+        self._connection.execute(
+            'INSERT INTO artists (name, genre) VALUES (%s, %s)', 
+            [artist.name, artist.genre]
+            )
         return None
 
     # Delete an artist by their id
     def delete(self, artist_id):
         self._connection.execute(
-            'DELETE FROM artists WHERE id = %s', [artist_id])
+            'DELETE FROM artists WHERE id = %s', 
+            [artist_id]
+            )
         return None
